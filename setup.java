798cabf7bfb44b29a0e0e6365b34a3acf9f1f431
SAC_CS112_Do_Xuan <xdo0410@gmail.com>
2014-11-11 21:15:10 -0800

    Create setup.java

---
+++ setup.java
@@ -0,0 +1,37 @@
+
+public class setup {
+
+	int user;
+	int num;
+	
+	public setup(int num){
+		setUser(num);
+	}
+		
+		public int getPlay()
+		{
+			return getUser();
+		}
+		
+		public void PolymorphismEndGame()
+		{
+		System.out.println("...");
+}
+		public void EnterValue(int num)
+		{
+			 if (num == 1) 
+				 num = 1; 
+			    else if (num == 2) 
+			    	num = 2; 
+			    else if (num == 3) 
+			    	num = 3; 
+		}
+
+		public int getUser() {
+			return user;
+		}
+
+		public void setUser(int user) {
+			this.user = user;
+			}
+		}
